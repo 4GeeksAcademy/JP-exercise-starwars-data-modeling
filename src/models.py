f235aa78be83397116a8311cca1d04cbf08cb71f
JPbonaldo <159311815+JPbonaldo@users.noreply.github.com>
2024-06-17 19:45:07 +0000

    in progress

--- src/models.py
+++ src/models.py
@@ -14,6 +14,65 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+class User (Base):
+    id = Column(Integer, primary_key = True)
+    username = Column(String(30), nullable = False)
+    password = Column(String(30), nullable = False)
+    email = Column(String, nullable = True)
+    def serialize(self):
+        return {
+            'username': self.username,
+            'email': self.email
+            
+        }
+    
+class Planet (Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(30), nullable = False)
+    population = Column(Integer, nullable = False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'population': self.population
+        }
+
+class Character (Base):
+    id = Column(Integer, primary_key = True)
+    name = Column(String(30), nullable = False)
+    age = Column(Integer, nullable = False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'age': self.age
+        }
+    
+class Vehicle (Base):
+    id = Column(Integer, primary_key = True)
+    name = Column(String(30), nullable = False)
+    top_speed = Column(Integer, nullable = False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'top_speed': self.top_speed
+        }
+    
+class FavoriteList (Base):
+    __tablename__ = 'favorite list'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    user = relationship(User)
+    planet = relationship(Planet)
+    character = relationship(Character)
+    vehicle = relationship(Vehicle)
+    def serialize(self):
+        return {
+            
+        }
+
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
